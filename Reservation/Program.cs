c1d195430fe2d93df2927b64d14a425c5a8b42a0
t2kob <4531723+t2-kob@users.noreply.github.com>
2019-12-25 00:35:34 +0900

    ADD 2019/12/21 モデリング会終了時点。コンパイルエラーとなります。

---
+++ Reservation/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Reservation {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Reservation/ReservationUseCase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reservation {
+    class ReservationUseCase {
+
+        public void Reserve(予約する時必要な情報 予約する時必要な情報のインスタンス) {
+           
+        }
+    }
+}

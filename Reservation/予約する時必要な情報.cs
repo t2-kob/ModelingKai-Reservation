9f1153ef69ad504ed0b3ec7022fb6e2f2b394e5d
Jun Nakajima <ikuta1919@gmail.com>
2020-01-02 13:24:53 +0900

    add: 予約する時に必要な情報の引数のクラスを作成した（赤線を消したよ）

--- Reservation/予約する時必要な情報.cs
+++ Reservation/予約する時必要な情報.cs
@@ -1,6 +1,6 @@
 ﻿namespace Reservation {
     public class 予約する時必要な情報 {
-        public 予約する時必要な情報(MeetingRoom room, ReserverId id, ReservationRange range, 予定人数 ninzu) { 
+        public 予約する時必要な情報(MeetingRoom room, ReserverId id, ReservationRange range, 想定使用人数 ninzu) { 
 
         }
     }

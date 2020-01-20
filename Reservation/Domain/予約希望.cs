7fd1294a133d57cdf3ba8078aa479ddc03ae0e10
t2kob <4531723+t2-kob@users.noreply.github.com>
2020-01-20 22:27:49 +0900

    MOD 入れ忘れ。

--- Reservation/Domain/予約希望.cs
+++ Reservation/Domain/予約希望.cs
@@ -1,7 +1,10 @@
 ﻿namespace Reservation.Domain {
-    public class 予約 {
-        public 予約(MeetingRoom room, ReserverId id,時間に関するなにがし range, 想定使用人数 ninzu) { 
+    public class 予約希望 {
+        public 予約希望(MeetingRoom room, ReserverId id,時間に関するなにがし range, 想定使用人数 ninzu) { 
 
+        }
+        public 予約結果 申請() {
+            return null;//TODO 返すのはプリミティブ？予約結果？
         }
     }
 }

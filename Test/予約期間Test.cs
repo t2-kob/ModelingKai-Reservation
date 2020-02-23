659fd138cd41292aef246d23efec16926baa2666
shibatea <ggl.shibata@gmail.com>
2020-02-23 23:25:32 +0900

    add: tests for 予約期間

--- Test/予約期間Test.cs
+++ Test/予約期間Test.cs
@@ -8,12 +8,17 @@ namespace Test
 
     public class 予約期間Test
     {
+        /// <summary>
+        /// 14:00~16:00
+        /// </summary>
         private static readonly 予約期間 _target = new 予約期間(new 予約年月日(2020, 2, 23), 予約開始_時._14, 予約開始_分._00, new コマ数(8));
 
         public class NGの場合
         {
-
-
+            /// <summary>
+            /// 対象 14:00~16:00   |====|
+            /// 自分 13:00~15:00 |====|
+            /// </summary>
             [Fact]
             public void 後ろが被るのでNG()
             {
@@ -22,75 +27,135 @@ namespace Test
                 Assert.True(result);
             }
 
+            /// <summary>
+            /// 対象 14:00~16:00   |====|
+            /// 自分 15:00~17:00      |====|
+            /// </summary>
             [Fact]
             public void 前が被るのでNG()
             {
-                Assert.True(false); // TODO: 以下全部の Assert.True(false); なんとかしよう
+                var me = new 予約期間(new 予約年月日(2020, 2, 23), 予約開始_時._15, 予約開始_分._00, new コマ数(8));
+                var result = me.時間かぶってますか(_target);
+                Assert.True(result);
             }
 
+            /// <summary>
+            /// 対象 14:00~16:00   |====|
+            /// 自分 14:30~15:30    |==|
+            /// </summary>
             [Fact]
             public void 包含されるのでNG()
             {
-                Assert.True(false);
+                var me = new 予約期間(new 予約年月日(2020, 2, 23), 予約開始_時._14, 予約開始_分._30, new コマ数(4));
+                var result = me.時間かぶってますか(_target);
+                Assert.True(result);
             }
 
+            /// <summary>
+            /// 対象 14:00~16:00   |====|
+            /// 自分 13:30~16:30 |========|
+            /// </summary>
             [Fact]
             public void 包含するのでNG()
             {
-                Assert.True(false);
+                var me = new 予約期間(new 予約年月日(2020, 2, 23), 予約開始_時._13, 予約開始_分._30, new コマ数(12));
+                var result = me.時間かぶってますか(_target);
+                Assert.True(result);
             }
 
+            /// <summary>
+            /// 境界値テスト
+            /// 対象 14:00~16:00   |====|
+            /// 自分 14:00~15:00   |==|
+            /// </summary>
             [Fact]
             public void 開始時刻が被るのでNG()
             {
-                Assert.True(false);
+                var me = new 予約期間(new 予約年月日(2020, 2, 23), 予約開始_時._14, 予約開始_分._00, new コマ数(4));
+                var result = me.時間かぶってますか(_target);
+                Assert.True(result);
             }
 
+            /// <summary>
+            /// 境界値テスト
+            /// 対象 14:00~16:00   |====|
+            /// 自分 15:00~16:00     |==|
+            /// </summary>
             [Fact]
             public void 終了時刻が被るのでNG()
             {
-                Assert.True(false);
+                var me = new 予約期間(new 予約年月日(2020, 2, 23), 予約開始_時._15, 予約開始_分._00, new コマ数(4));
+                var result = me.時間かぶってますか(_target);
+                Assert.True(result);
             }
 
+            /// <summary>
+            /// 境界値テスト
+            /// 対象 14:00~16:00   |====|
+            /// 自分 14:00~16:00   |====|
+            /// </summary>
             [Fact]
             public void 開始時刻と終了時刻が両方被るのでNG()
             {
-                Assert.True(false);
+                var me = new 予約期間(new 予約年月日(2020, 2, 23), 予約開始_時._14, 予約開始_分._00, new コマ数(8));
+                var result = me.時間かぶってますか(_target);
+                Assert.True(result);
             }
-
-
-
         }
 
         public class 手前側のOK
         {
+            /// <summary>
+            /// 対象 14:00~16:00    |====|
+            /// 自分 13:00~13:30 |=|
+            /// </summary>
             [Fact]
             public void 終了時刻が_対象の開始時刻より前だからOK()
             {
-                var me = new 予約期間(new 予約年月日(2020, 2, 23), 予約開始_時._10, 予約開始_分._00, new コマ数(8));
+                var me = new 予約期間(new 予約年月日(2020, 2, 23), 予約開始_時._13, 予約開始_分._30, new コマ数(2));
                 var result = me.時間かぶってますか(_target);
                 Assert.False(result);
-
             }
+
+            /// <summary>
+            /// 境界値テスト
+            /// 対象 14:00~16:00    |====|
+            /// 自分 13:00~14:00 |==|
+            /// </summary>
             [Fact]
             public void 終了時刻と_対象の開始時刻が同じだけどOK()
             {
-                Assert.True(false);
+                var me = new 予約期間(new 予約年月日(2020, 2, 23), 予約開始_時._13, 予約開始_分._00, new コマ数(4));
+                var result = me.時間かぶってますか(_target);
+                Assert.False(result);
             }
 
         }
         public class 後ろ側のOK
         {
-
+            /// <summary>
+            /// 対象 14:00~16:00    |====|
+            /// 自分 16:30~17:00          |=|
+            /// </summary>
             [Fact]
             public void 開始時刻が_対象の終了時刻より後だからOK()
             {
-                Assert.True(false);
+                var me = new 予約期間(new 予約年月日(2020, 2, 23), 予約開始_時._16, 予約開始_分._30, new コマ数(2));
+                var result = me.時間かぶってますか(_target);
+                Assert.False(result);
             }
+
+            /// <summary>
+            /// 境界値テスト
+            /// 対象 14:00~16:00    |====|
+            /// 自分 16:00~17:00         |==|
+            /// </summary>
             [Fact]
             public void 開始時刻と_対象の終了時刻が同じだけどOK()
             {
-                Assert.True(false);
+                var me = new 予約期間(new 予約年月日(2020, 2, 23), 予約開始_時._16, 予約開始_分._00, new コマ数(4));
+                var result = me.時間かぶってますか(_target);
+                Assert.False(result);
             }
         }
     }

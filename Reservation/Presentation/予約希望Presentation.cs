fdea9269624becb9b0e90ecd9e811cd9a62c01a4
t2-kob <4531723+t2-kob@users.noreply.github.com>
2020-03-22 22:10:11 +0900

    MOD 次回、予約希望 Presentation をやっつける？ (振り返りの結果を受けて、今後どうするか決める予定。)

--- Reservation/Presentation/予約希望Presentation.cs
+++ Reservation/Presentation/予約希望Presentation.cs
@@ -15,12 +15,12 @@ namespace Reservation.Presentation
         {
             //TODO: Presentation層の実装。
 
-            //var 希望 = new 予約希望(new MeetingRoom(roomName),
-            //                        new ReserverId(reserverId),
-            //                        new 予約期間(new 予約年月日(start.Year, start.Month, ....), ...)
-            //                        new 想定使用人数());
+            var 希望 = new 予約希望(new MeetingRoom(roomName),
+                                    new ReserverId(reserverId),
+                                    new 予約期間(new 予約年月日(start.Year, start.Month, ....), ...)
+                                    new 想定使用人数());
 
-            //new ReservationUseCase().予約する(希望);
+            new ReservationUseCase().予約する(希望);
         }
 
     }

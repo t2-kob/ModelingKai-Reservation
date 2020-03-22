38a32aaec0503d2114e3833da2aac9e50eaaf6a4
t2-kob <4531723+t2-kob@users.noreply.github.com>
2020-03-22 21:59:38 +0900

    ADD Presentationクラスをとりあえず作った(コンパイルはまだ通らない)。

--- Reservation/Presentation/予約希望Presentation.cs
+++ Reservation/Presentation/予約希望Presentation.cs
@@ -11,29 +11,13 @@ namespace Reservation.Presentation
     public class 予約希望Presentation
     {
 
-        public void 予約1() {
-            new ReservationUseCase().予約する(new MeetingRoom(roomName), 
-                                              new ReserverId(reserverId),
-                                              new 予約期間(new 予約年月日(start.Year, start.Month, .... ), ...)
-                                              new 想定使用人数());
-        }
-        public void 予約2()
+        public void 予約()
         {
             var 希望 = new 予約希望(new MeetingRoom(roomName),
                                     new ReserverId(reserverId),
                                     new 予約期間(new 予約年月日(start.Year, start.Month, ....), ...)
                                     new 想定使用人数());
 
-            new ReservationUseCase().予約する(希望);
-        }
-        public void 予約3()
-        {
-            new ReservationUseCase().予約する(roomName, reserverId, DateTime start, DateTime end, ...);
-        }
-
-        public void 予約4()
-        {
-            予約希望 希望 = new 予約希望つくるUseCase(roomName, reserverId, DateTime start, DateTime end, ...);
             new ReservationUseCase().予約する(希望);
         }
 

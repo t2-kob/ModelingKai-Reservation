73e0529d71b15646342e030b20658b859b3804d9
t2-kob <4531723+t2-kob@users.noreply.github.com>
2020-05-03 22:34:09 +0900

    ADD 次回、Presentation層に入る！！　プロジェクト『Cli』 を実装する。DI や ConsoleApp 系のフレームワークの導入を検討するところから。

---
+++ Presentations/Cli/Cli/Program.cs
@@ -0,0 +1,23 @@
+﻿using Reservation.Infrastructure;
+using Reservation.Usecase;
+using System;
+using System.Diagnostics;
+
+namespace Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Infrastracture 層見えちゃってるけど、また考える。
+            //       ==> DI する？ するとしたら何でやる？
+            //              ==> ASP.Net 純正のDIのやつ、SimpleInjection、とかとか。
+            //
+            // TODO: ConsoleApp 系のフレームワーク使う？
+            var usecase = new ReservationUseCase(new 予約希望Repository());
+
+            Console.WriteLine("Hello World!");
+            Debug.WriteLine("Hello World!");
+        }
+    }
+}
--- Reservation/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Reservation {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}

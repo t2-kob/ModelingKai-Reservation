c61604cc19b7cc1ae37af6e4f587685c0ef62228
t2-kob <4531723+t2-kob@users.noreply.github.com>
2020-05-10 22:32:37 +0900

    WIP  ※ エラーになります。　　Exception を投げたりキャッチしたりするところから、次回頑張る。そのあと永続化について考える。

--- Presentations/Cli/Application.cs
+++ Presentations/Cli/Application.cs
@@ -47,17 +47,41 @@ namespace Cli
         }
 
         private 予約希望 予約希望つくる(string[] args) {
-            var meetingRoom = new MeetingRoom((MeetingRoomName)Enum.Parse(typeof(MeetingRoomName), args[0]));
-            var 予約開始DateTime = DateTime.Parse(args[1]);
-            var 予約終了DateTime = DateTime.Parse(args[2]);
 
-            予約開始日時 予約開始日時 = 予約時間Parser.予約開始日時をつくる(予約開始DateTime);
-            予約終了日時 予約終了日時 = 予約時間Parser.予約終了日時をつくる(予約終了DateTime);
+            // TODO: パース(下準備) と、予約希望を作るところを分ける？
+            // TODO: meetingRoom, 予約開始DateTime, 予約終了DateTime だけをパースとして分離する？
+            // TODO: UIExceptionと、ドメインのExceptionとかを作る。 
+            //           ==> (ここ→は改めて決定)層ごとに抽象例外クラスを作って、具体的な個々の例外はそのサブクラスにすると扱いやすいと思ってる。
+            // TODO2: SQLite 入れるとか、永続化に関することも今後やりたい。
+            try
+            {
+                var meetingRoom = new MeetingRoom((MeetingRoomName)Enum.Parse(typeof(MeetingRoomName), args[0])); //TODO: TryParse() にする？
+                var 予約開始DateTime = DateTime.Parse(args[1]);                                                   // 同上
+                var 予約終了DateTime = DateTime.Parse(args[2]);
 
-            return new 予約希望(meetingRoom,
-                                new ReserverId(),
-                                new 予約期間(予約開始日時, 予約終了日時),
-                                new 想定使用人数());
+                予約開始日時 予約開始日時 = 予約時間Parser.予約開始日時をつくる(予約開始DateTime);
+                予約終了日時 予約終了日時 = 予約時間Parser.予約終了日時をつくる(予約終了DateTime);
+            }
+            catch (Exception e) {
+                throw new UI入出力おかしいぞException(e);
+            }
+            
+
+           
+            try
+            {
+                var 予約希望 = new 予約希望(meetingRoom,
+                                     new ReserverId(),
+                                     new 予約期間(予約開始日時, 予約終了日時),
+                                     new 想定使用人数());
+
+                return 予約希望;
+            }
+            catch(ドメインのException e) {
+                // TODO: このExceptionは、予約希望が投げるべきじゃない？
+                throw new ドメインのException(e);
+            }
+
         }
 
 

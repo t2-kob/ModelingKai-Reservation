8a6c06bb9c77f36f6529ee19138c249b7b3dc6c1
t2-kob <4531723+t2-kob@users.noreply.github.com>
2020-05-10 21:55:42 +0900

    MOD 予約時間Parser を作った。

--- Presentations/Cli/Application.cs
+++ Presentations/Cli/Application.cs
@@ -22,25 +22,14 @@ namespace Cli
         /// </summary>
         protected override void Main(string[] args)
         {
-            // TODO: args をパースするところからやる？
-
             // args => string[3] ===> B , "2020-05-10T12:00:00+09:00", "2020-05-10T13:00:00+09:00"
-
             var meetingRoom = new MeetingRoom((MeetingRoomName)Enum.Parse(typeof(MeetingRoomName), args[0]));
             var 予約開始DateTime = DateTime.Parse(args[1]);
             var 予約終了DateTime = DateTime.Parse(args[2]);
 
-            var 予約開始年月日 = new 予約年月日(予約開始DateTime.Year, 予約開始DateTime.Month, 予約開始DateTime.Day);
-            var 予約開始時 = (予約開始_時)Enum.Parse(typeof(予約開始_時), 予約開始DateTime.Hour.ToString());
-            var 予約開始分 = (予約_分)Enum.Parse(typeof(予約_分), 予約開始DateTime.Minute.ToString());
-            var 予約開始日時 = new 予約開始日時(予約開始年月日, 予約開始時, 予約開始分);
 
-
-            var 予約終了年月日 = new 予約年月日(予約終了DateTime.Year, 予約終了DateTime.Month, 予約終了DateTime.Day);
-            var 予約終了時 = (予約終了_時)Enum.Parse(typeof(予約終了_時), 予約終了DateTime.Hour.ToString());
-            var 予約終了分 = (予約_分)Enum.Parse(typeof(予約_分), 予約終了DateTime.Minute.ToString());
-            var 予約終了日時 = new 予約終了日時(予約終了年月日, 予約終了時, 予約終了分);
-
+            予約開始日時 予約開始日時 = 予約時間Parser.予約開始日時をつくる(予約開始DateTime);
+            予約終了日時 予約終了日時 = 予約時間Parser.予約終了日時をつくる(予約終了DateTime);
 
             var usecase = new ReservationUseCase(_repository);
 
@@ -55,10 +44,11 @@ namespace Cli
 
 
             Debug.WriteLine($"予約成功したかどうか？ ==> {予約成功したか}");
-            
+
 
 
             Debug.WriteLine("Hello World!");
         }
+
     }
 }
---
+++ Presentations/Cli/予約時間Parser.cs
@@ -0,0 +1,26 @@
+﻿using Reservation.Domain.Reservations.Period;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cli
+{
+    public static class 予約時間Parser
+    {
+        public static 予約終了日時 予約終了日時をつくる(DateTime input)
+        {
+            var 予約終了年月日 = new 予約年月日(input.Year, input.Month, input.Day);
+            var 予約終了時 = (予約終了_時)Enum.Parse(typeof(予約終了_時), input.Hour.ToString());
+            var 予約終了分 = (予約_分)Enum.Parse(typeof(予約_分), input.Minute.ToString());
+            return new 予約終了日時(予約終了年月日, 予約終了時, 予約終了分);
+        }
+
+        public static 予約開始日時 予約開始日時をつくる(DateTime input)
+        {
+            var 予約開始年月日 = new 予約年月日(input.Year, input.Month, input.Day);
+            var 予約開始時 = (予約開始_時)Enum.Parse(typeof(予約開始_時), input.Hour.ToString());
+            var 予約開始分 = (予約_分)Enum.Parse(typeof(予約_分), input.Minute.ToString());
+            return new 予約開始日時(予約開始年月日, 予約開始時, 予約開始分);
+        }
+    }
+}

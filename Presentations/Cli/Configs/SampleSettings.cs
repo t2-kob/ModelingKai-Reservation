d263e2763ae16f4f8540f74caafafdac62453f51
shibatea <ggl.shibata@gmail.com>
2020-05-25 02:38:32 +0900

    feat: DI設定について Docs を参照して Host.CreateDefaultBuilder を利用する方式に変更。設定ファイルでログ出力レベルを設定できるように変更。

---
+++ Presentations/Cli/Configs/SampleSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cli.Configs
+{
+    public class SampleSettings
+    {
+        public string Key { get; set; }
+    }
+}

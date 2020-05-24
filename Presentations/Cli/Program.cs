24919979242cf9bcf5bbd2b144602b572d5dfc9f
shibatea <ggl.shibata@gmail.com>
2020-05-25 04:34:07 +0900

    feat: DI 設定クラス Startup.cs を追加（ASP.NET Core 風）

--- Presentations/Cli/Program.cs
+++ Presentations/Cli/Program.cs
@@ -1,60 +1,17 @@
-﻿using System;
-using System.IO;
+﻿using Cli.Applications;
 using Microsoft.Extensions.DependencyInjection;
-using Cli.Applications;
-// using Cli.Configs;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Reservation.Domain.Reservations;
-using Reservation.Infrastructure;
 
 namespace Cli
 {
     class Program
     {
-        private static string Env =>
-#if DEBUG
-            "Development";
-#else
-            "Production";
-#endif
-
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            var host = CreateHostBuilder().Build();
+            var host = Startup.CreateHostBuilder().Build();
 
             var application = host.Services.GetRequiredService<IApplication>();
 
             application.Run(args);
         }
-
-        private static IHostBuilder CreateHostBuilder() =>
-            Host.CreateDefaultBuilder()
-                .ConfigureLogging(logging =>
-                {
-                    // ログ機能のDI設定
-                    logging.AddConsole();
-                    logging.AddDebug();
-                })
-                .ConfigureAppConfiguration(builder =>
-                {
-                    // 設定ファイル読み込み
-                    builder.SetBasePath(Directory.GetCurrentDirectory());
-                    builder.AddJsonFile("appsettings.json");
-                    builder.AddJsonFile($"appsettings.{Env}.json");
-                    builder.AddEnvironmentVariables();
-                })
-                .ConfigureServices((context, collection) =>
-                {
-                    // SampleSettingsのDI設定
-                    // collection.Configure<SampleSettings>(context.Configuration.GetSection(nameof(SampleSettings)));
-
-                    // RepositoryのDI設定
-                    collection.AddTransient<I予約希望Repository, 予約希望Repository>();
-
-                    // ApplicationのDI設定
-                    collection.AddTransient<IApplication, Application>();
-                });
     }
 }
---
+++ Presentations/Cli/Startup.cs
@@ -0,0 +1,49 @@
+using System.IO;
+using Cli.Applications;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Reservation.Domain.Reservations;
+using Reservation.Infrastructure;
+
+namespace Cli
+{
+    internal static class Startup
+    {
+        private static string Env =>
+#if DEBUG
+            "Development";
+#else
+            "Production";
+#endif
+
+        public static IHostBuilder CreateHostBuilder() =>
+            Host.CreateDefaultBuilder()
+                .ConfigureLogging(logging =>
+                {
+                    // ログ機能のDI設定
+                    logging.AddConsole();
+                    logging.AddDebug();
+                })
+                .ConfigureAppConfiguration(builder =>
+                {
+                    // 設定ファイル読み込み
+                    builder.SetBasePath(Directory.GetCurrentDirectory());
+                    builder.AddJsonFile("appsettings.json");
+                    builder.AddJsonFile($"appsettings.{Env}.json");
+                    builder.AddEnvironmentVariables();
+                })
+                .ConfigureServices((context, collection) =>
+                {
+                    // SampleSettingsのDI設定
+                    // collection.Configure<SampleSettings>(context.Configuration.GetSection(nameof(SampleSettings)));
+
+                    // RepositoryのDI設定
+                    collection.AddTransient<I予約希望Repository, 予約希望Repository>();
+
+                    // ApplicationのDI設定
+                    collection.AddTransient<IApplication, Application>();
+                });
+    }
+}

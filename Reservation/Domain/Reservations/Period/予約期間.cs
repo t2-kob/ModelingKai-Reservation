342916c14ae993c2c1539ecc785138b41914bf1b
t2-kob <4531723+t2-kob@users.noreply.github.com>
2020-07-12 22:03:45 +0900

    もいっこTODOきえた。

--- Reservation/Domain/Reservations/Period/予約期間.cs
+++ Reservation/Domain/Reservations/Period/予約期間.cs
@@ -4,8 +4,6 @@ using System;
 namespace Reservation.Domain.Reservations.Period {
     public class 予約期間 {
 
-        //TODO: ルール: 日をまたいではいけない　とかあるよ。
-
         private readonly 予約開始日時 _開始日時;
         private readonly 予約終了日時 _終了日時;
 
